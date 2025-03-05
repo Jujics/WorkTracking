d7438cf80197c24322f7e66392ac6c8f82a41ad5
Juji.cs <61978075+Jujics@users.noreply.github.com>
2025-03-05 19:45:07 +0100

    Got the time working

---
+++ WorkTrackingForDavd/TimeViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+using System;
+using System.ComponentModel;
+
+namespace WorkTrackingForDavd;
+
+public class TimeViewModel : INotifyPropertyChanged
+{
+    private DateTime _currentTime;
+    private DispatcherTimer _timer;
+
+    public TimeViewModel()
+    {
+        _currentTime = DateTime.Now;
+        
+        _timer = new DispatcherTimer();
+        _timer.Interval = TimeSpan.FromSeconds(1);
+        _timer.Tick += Timer_Tick;
+        _timer.Start();
+    }
+
+    private void Timer_Tick(object? sender, EventArgs e)
+    {
+        CurrentTime = DateTime.Now;
+    }
+
+    public DateTime CurrentTime
+    {
+        get { return _currentTime; }
+        set
+        {
+            _currentTime = value;
+            OnPropertyChanged(nameof(CurrentTimeString));
+        }
+    }
+
+    public String CurrentTimeString
+    {
+        get { return _currentTime.ToString("HH:mm:ss"); }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(String propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
